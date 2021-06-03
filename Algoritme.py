2ba175b2e4bd3fe52c83d35a2abe6e173c3eff57
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-03 14:59:30 +0200

    Update Algoritme.py
    Probeer_stok() toegevoegd

--- Algoritme.py
+++ Algoritme.py
@@ -1,15 +1,31 @@
 from random import shuffle
 
+
+def probeer_stok(bouwstapels, stok):
+    print(stok)
+    for x in bouwstapels:
+        print(bouwstapels[x])
+        if (len(bouwstapels[x]) == 0 and (stok[0] == "1" or stok[0] == "SB")) or (len(bouwstapels[x]) > 0 and bouwstapels[x][len(bouwstapels[x]) - 1] + 1 == stok[0]):
+            bouwstapels[x].append(stok[0])
+            stok = stok[1:]
+    print(stok)
+    print(bouwstapels)
+
+
 trekstapel = [str(x % 12 + 1) for x in range(0, 144)]
 trekstapel += ["SB"] * 18
 shuffle(trekstapel)
 
-print(trekstapel)
+bouwstapels = {'A': [], 'B': [], 'C': [], 'D': []}
+
+stok = trekstapel[:30]
+trekstapel = trekstapel[30:]
 
 kaarten = trekstapel[:5]
 trekstapel = trekstapel[5:]
 
-print(kaarten)
-print(trekstapel)
+probeer_stok(bouwstapels, stok)
 
-bouwstapels = {'A': [], 'B': [], 'C': [], 'D': []}
+
+
+
