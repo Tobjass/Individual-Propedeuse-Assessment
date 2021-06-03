0f2025a26abdad7e6c2cc634835eee07a114300f
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-03 15:28:37 +0200

    Update Algoritme.py
    probeer_hand() toegevoegd

--- Algoritme.py
+++ Algoritme.py
@@ -1,18 +1,35 @@
 from random import shuffle
 
-
 def probeer_stok(bouwstapels, stok):
+    verandering = False
     print(stok)
     for x in bouwstapels:
         print(bouwstapels[x])
-        if (len(bouwstapels[x]) == 0 and (stok[0] == "1" or stok[0] == "SB")) or (len(bouwstapels[x]) > 0 and bouwstapels[x][len(bouwstapels[x]) - 1] + 1 == stok[0]):
+        if ((len(bouwstapels[x]) == 0 and stok[0] == 1) or (len(bouwstapels[x]) > 0 and bouwstapels[x][-1] + 1 == stok[0])) or stok[0] == "SB":
             bouwstapels[x].append(stok[0])
             stok = stok[1:]
-    print(stok)
-    print(bouwstapels)
+            verandering = True
+    return bouwstapels, stok, verandering
 
+def probeer_hand(bouwstapels, hand):
+    verandering = False
+    print(hand)
+    for x in bouwstapels:
+        print(bouwstapels[x])
+        index = None
+        if "SB" in hand:
+            index = hand.index("SB")
+        elif len(bouwstapels[x]) == 0 and 1 in hand:
+            index = hand.index(1)
+        elif len(bouwstapels[x]) > 0 and bouwstapels[x][-1] + 1 in hand:
+            index = hand.index(bouwstapels[x][len(bouwstapels[x]) - 1] + 1)
+        if index is not None:
+            bouwstapels[x].append(hand[index])
+            hand = hand[:index] + hand[index+1:]
+            verandering = True
+    return bouwstapels, hand, verandering
 
-trekstapel = [str(x % 12 + 1) for x in range(0, 144)]
+trekstapel = [x % 12 + 1 for x in range(0, 144)]
 trekstapel += ["SB"] * 18
 shuffle(trekstapel)
 
@@ -21,10 +38,16 @@ bouwstapels = {'A': [], 'B': [], 'C': [], 'D': []}
 stok = trekstapel[:30]
 trekstapel = trekstapel[30:]
 
-kaarten = trekstapel[:5]
+hand = trekstapel[:5]
 trekstapel = trekstapel[5:]
 
-probeer_stok(bouwstapels, stok)
+bouwstapels, stok, verandering = probeer_stok(bouwstapels, stok)
+
+print(bouwstapels, verandering)
+
+bouwstapels, hand, verandering = probeer_hand(bouwstapels, hand)
+
+print(bouwstapels, verandering)
 
 
 
