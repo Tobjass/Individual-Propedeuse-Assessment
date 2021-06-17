273c594676a999163b9c0f4160ae0accfe1c6dc0
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-17 13:09:47 +0200

    Update GUI.py
    For loop voor het aanmaken van de stapels in een functie gezet om het aantal kaarten in de hand toe te kunnen voegen

--- GUI.py
+++ GUI.py
@@ -1,21 +1,23 @@
 import tkinter as tk
 
-window = tk.Tk()
-
-for speler in range(1, 3):
-    window.rowconfigure(speler, weight=1, minsize=50)
-
-    for stapel in range(1, 6):
+def stapels_aanmaken(speler, hand_aantal):
+    for stapel in range(1, 6 + hand_aantal):
         window.columnconfigure(stapel, weight=1, minsize=75)
         frame = tk.Frame(
             master=window,
             relief=tk.RAISED,
             borderwidth=1
         )
-        frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1) else 5, pady=40, ipadx=40, ipady=80,
-                   sticky="n" if (speler == 1) else "s")
+        frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1) else 5, pady=40, ipadx=40, ipady=80, sticky="n" if (speler == 1) else "s")
 
         label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, stapel))
         label.pack(padx=5, pady=5)
+
+window = tk.Tk()
+
+for speler in range(1, 3):
+    window.rowconfigure(speler, weight=1, minsize=50)
+
+    stapels_aanmaken(speler, 1)
 
 window.mainloop()
