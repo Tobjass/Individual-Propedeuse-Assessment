d17eafacaf8240d5a93675779708d6a195d02570
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-17 16:46:21 +0200

    Update GUI.py
    Stok zal nu vanaf 5 kaarten visueel laten zien hoeveel er nog zijn

--- GUI.py
+++ GUI.py
@@ -1,42 +1,50 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
-def run(window, aantal_kaarten_hand):
+def run(window, bovenste_kaart_stok, aantal_kaarten_stok, aantal_kaarten_hand):
     window.configure(background="#d6e0f5")
 
     for speler in range(1, 4):
         window.rowconfigure(speler, weight=1, minsize=50)
 
-        stapels_aanmaken(window, speler, (6 + aantal_kaarten_hand if (speler != 2) else 6) if (speler < 3) else 1,
-                         1 if (speler < 3) else (6 + aantal_kaarten_hand if (speler != 2) else 6), -1 if (speler < 3) else 1)
+        stapels_aanmaken(window, speler, bovenste_kaart_stok, aantal_kaarten_stok, 7 + aantal_kaarten_hand,
+                         (6 + aantal_kaarten_hand if (speler != 2) else 6) if (speler < 3) else 1,
+                         1 if (speler < 3) else (6 + aantal_kaarten_hand if (speler != 2) else 6),
+                         -1 if (speler < 3) else 1)
 
     window.mainloop()
 
-def stapels_aanmaken(window, speler, start, stop, step):
+def stapels_aanmaken(window, speler, bovenste_kaart_stok, aantal_kaarten_stok, aantal_kaarten_hand, start, stop, step):
     for stapel in range(start, stop, step):
         window.columnconfigure(stapel, weight=1, minsize=75)
+
+        temp_stapel = aantal_kaarten_hand - stapel if (speler != 3) else stapel
 
         if speler == 2 and stapel == 6:
             image = ImageTk.PhotoImage(Image.open("Images/Trekstapel.png"))
 
             label = tk.Label(window, image=image, background="#d6e0f5")
             label.image = image
-            label.grid(row=speler, column=stapel)
-        elif speler != 2 and (7 - stapel if (speler != 3) else stapel) == 1:
-            image = ImageTk.PhotoImage(Image.open("Images/1/1.png"))
+            label.grid(row=speler, column=stapel, padx=100)
+        elif speler != 2 and temp_stapel == 1:
+            image = ImageTk.PhotoImage(Image.open(
+                "Images/{}/{} ({}).png".format(bovenste_kaart_stok, bovenste_kaart_stok,
+                                               5 if (aantal_kaarten_stok > 5) else aantal_kaarten_stok)))
 
             label = tk.Label(window, image=image, background="#d6e0f5")
             label.image = image
-            label.grid(row=speler, column=stapel, pady=40, sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
+            label.grid(row=speler, column=stapel, padx=100, pady=40,
+                       sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
         else:
             frame = tk.Frame(
                 master=window,
                 relief=tk.RAISED,
                 borderwidth=1,
             )
-            frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1 or stapel == 6) else 5, pady=40, ipadx=40, ipady=80, sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
+            frame.grid(row=speler, column=stapel, padx=5, pady=40, ipadx=40, ipady=80,
+                       sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
 
-            label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, 7 - stapel if (speler != 3) else stapel))
+            label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, temp_stapel))
             label.pack(padx=5, pady=5)
 
-run(tk.Tk(className=' Skip-Bo'), 1)
+run(tk.Tk(className=' Skip-Bo'), 1, 30, 0)
