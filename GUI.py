0dac1a62ec24ae0e9a2e611b9e86ee61df2039a3
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-17 13:52:07 +0200

    Update GUI.py
    For loop voor het aanmaken van de spelers in een functie gezet om globals weg te halen
    3e rij stapels toegevoegd voor bouwstapels en trekstapel

--- GUI.py
+++ GUI.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 
-def stapels_aanmaken(speler, hand_aantal):
-    for stapel in range(1, 6 + hand_aantal):
+def run(window, aantal_kaarten_hand):
+    window.configure(background="#d6e0f5")
+
+    for speler in range(1, 4):
+        window.rowconfigure(speler, weight=1, minsize=50)
+
+        stapels_aanmaken(window, speler, (6 + aantal_kaarten_hand if (speler != 2) else 6) if (speler < 3) else 1,
+                         1 if (speler < 3) else (6 + aantal_kaarten_hand if (speler != 2) else 6), -1 if (speler < 3) else 1)
+
+    window.mainloop()
+
+def stapels_aanmaken(window, speler, start, stop, step):
+    for stapel in range(start, stop, step):
         window.columnconfigure(stapel, weight=1, minsize=75)
         frame = tk.Frame(
             master=window,
             relief=tk.RAISED,
-            borderwidth=1
+            borderwidth=1,
         )
-        frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1) else 5, pady=40, ipadx=40, ipady=80, sticky="n" if (speler == 1) else "s")
+        frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1 or stapel == 6) else 5, pady=40, ipadx=40, ipady=80, sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
 
-        label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, stapel))
+        label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, 7 - stapel if (speler == 1) else stapel))
         label.pack(padx=5, pady=5)
 
-window = tk.Tk()
-
-for speler in range(1, 3):
-    window.rowconfigure(speler, weight=1, minsize=50)
-
-    stapels_aanmaken(speler, 1)
-
-window.mainloop()
+run(tk.Tk(className=' Skip-Bo'), 1)
