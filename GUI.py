f522dcbc8518adff7edc1bd38e8d7241434a8d47
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-17 13:08:48 +0200

    Update GUI.py
    Stok stapel toegevoegd

--- GUI.py
+++ GUI.py
@@ -5,16 +5,17 @@ window = tk.Tk()
 for speler in range(1, 3):
     window.rowconfigure(speler, weight=1, minsize=50)
 
-    for stapel in range(1, 5):
+    for stapel in range(1, 6):
         window.columnconfigure(stapel, weight=1, minsize=75)
         frame = tk.Frame(
             master=window,
             relief=tk.RAISED,
             borderwidth=1
         )
-        frame.grid(row=speler, column=stapel, padx=5, pady=5, ipadx=40, ipady=80, sticky="n" if (speler == 1) else "s")
+        frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1) else 5, pady=40, ipadx=40, ipady=80,
+                   sticky="n" if (speler == 1) else "s")
 
-        label = tk.Label(master=frame, text=f"Speler {speler}\nStapel {stapel}")
+        label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, stapel))
         label.pack(padx=5, pady=5)
 
 window.mainloop()
