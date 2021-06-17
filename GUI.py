a7ba46aab303ddd36376073ed519bb1332a79962
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-17 14:57:59 +0200

    Update GUI.py
    Trekstapel image toegevoegd

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 def run(window, aantal_kaarten_hand):
     window.configure(background="#d6e0f5")
@@ -14,14 +15,23 @@ def run(window, aantal_kaarten_hand):
 def stapels_aanmaken(window, speler, start, stop, step):
     for stapel in range(start, stop, step):
         window.columnconfigure(stapel, weight=1, minsize=75)
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1,
-        )
-        frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1 or stapel == 6) else 5, pady=40, ipadx=40, ipady=80, sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
 
-        label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, 7 - stapel if (speler == 1) else stapel))
-        label.pack(padx=5, pady=5)
+        if speler == 2 and stapel == 6:
+            image = Image.open("Trekstapel.png")
+            photo = ImageTk.PhotoImage(image)
+
+            label = tk.Label(window, image=photo, background="#d6e0f5")
+            label.image = photo
+            label.grid(row=speler, column=stapel)
+        else:
+            frame = tk.Frame(
+                master=window,
+                relief=tk.RAISED,
+                borderwidth=1,
+            )
+            frame.grid(row=speler, column=stapel, padx=100 if (stapel == 1 or stapel == 6) else 5, pady=40, ipadx=40, ipady=80, sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
+
+            label = tk.Label(master=frame, text="Speler {}\nStapel {}".format(speler, 7 - stapel if (speler != 3) else stapel))
+            label.pack(padx=5, pady=5)
 
 run(tk.Tk(className=' Skip-Bo'), 1)
