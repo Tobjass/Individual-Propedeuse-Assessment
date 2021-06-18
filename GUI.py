55d78b5ac1fb5a72f3feac49bcfc7dbc6ecfe605
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-18 12:47:21 +0200

    Update GUI.py
    hand_aanmaken() toegevoegd

--- GUI.py
+++ GUI.py
@@ -9,6 +9,9 @@ def run(window, bovenste_kaart_stok, aantal_kaarten_stok, aantal_kaarten_hand):
 
         stapels_aanmaken(window, speler, bovenste_kaart_stok, aantal_kaarten_stok, 7 + aantal_kaarten_hand, 1, 9, 1)
 
+        if speler != 2:
+            hand_aanmaken(window, speler, [2, 4, 6, 9, "SB"])
+
     window.mainloop()
 
 def open_image(window, bestand, rotate, row, column, padx, pady, sticky):
@@ -45,5 +48,14 @@ def stapels_aanmaken(window, speler, bovenste_kaart_stok, aantal_kaarten_stok, a
         else:
             open_image(window, "Images/{}/{} ({}).png".format(stapel, stapel, 1), True if (speler == 1) else False,
                        speler, stapel, 5, 40, "n" if (speler == 1) else (None if (speler == 2) else "s"))
+
+def hand_aanmaken(window, speler, hand):
+    for kaart in range(1, len(hand) + 1):
+        image = Image.open("Images/{}/{} ({}).png".format(hand[kaart - 1], hand[kaart - 1], 1))
+        image = ImageTk.PhotoImage(image.rotate(180 if (speler == 1) else 0))
+
+        label = tk.Label(window, image=image, background="#d6e0f5")
+        label.image = image
+        label.place(x=70 * kaart if (speler == 1) else 1320 + 73 * kaart, y=40 if (speler == 1) else 780)
 
 run(tk.Tk(className=' Skip-Bo'), 11, 30, 0)
