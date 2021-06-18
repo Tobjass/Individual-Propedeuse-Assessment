f65e3c8d43104a67a61ed1988d0d27360ad94284
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-18 17:09:12 +0200

    Update GUI.py
    Rand van kaarten waar mens toegang tot heeft worden nu rood met hover

--- GUI.py
+++ GUI.py
@@ -3,6 +3,10 @@ from PIL import ImageTk, Image
 from random import shuffle
 
 # Grafical User Interface
+def hover(label, on_hover, on_leave):
+    label.bind("<Enter>", func=lambda x: label.config(background=on_hover))
+    label.bind("<Leave>", func=lambda x: label.config(background=on_leave))
+
 def open_image(window, bestand, rotate, setting, row, column, padx, pady, sticky):
     image = Image.open(bestand)
     image = ImageTk.PhotoImage(image.rotate(180 if (rotate) else 0))
@@ -13,6 +17,7 @@ def open_image(window, bestand, rotate, setting, row, column, padx, pady, sticky
         label.grid(row=row, column=column, padx=padx, pady=pady, sticky=sticky)
     elif setting == "place":
         label.place(x=70 * row if (column == 1) else 1320 + 73 * row, y=40 if (column == 1) else 780)
+    return label
 
 def stapels_maken(window, speler, stapels, stok):
     for column in range(1, 9):
@@ -36,25 +41,28 @@ def stapels_maken(window, speler, stapels, stok):
             open_image(window, "Images/Trekstapel.png", False, "grid", speler, column, 100, None, None)
         # Stok
         elif speler != 2 and temp_column == 2:
-            open_image(window, "Images/{}/{} ({}).png".format(stok[0], stok[0], 5 if (len(stok) > 5) else len(stok)),
+            label = open_image(window, "Images/{}/{} ({}).png".format(stok[0], stok[0], 5 if (len(stok) > 5) else len(stok)),
                        True if (speler == 1) else False, "grid", speler, column, 100, 40,
                        "n" if (speler == 1) else (None if (speler == 2) else "s"))
         # Bouw en weggooistapels
         else:
-            keys = ['A', 'B', 'C', 'D']
-            stapel = stapels[keys[6 - (9 - column if (speler != 1) else column)]]
-            open_image(window, "Images/{}".format(
+            stapel = stapels[['A', 'B', 'C', 'D'][6 - (9 - column if (speler != 1) else column)]]
+            label = open_image(window, "Images/{}".format(
                 "Lege stapel.png" if (len(stapel) == 0) else "{}/{} ({}).png".format(stapel[-1], stapel[-1],
                                                                                      5 if (len(stapel) > 5) else len(
                                                                                          stapel))),
-                       True if (speler == 1) else False, "grid", speler, column, 5, 40,
-                       "n" if (speler == 1) else (None if (speler == 2) else "s"))
+                               True if (speler == 1) else False, "grid", speler, column, 5, 40,
+                               "n" if (speler == 1) else (None if (speler == 2) else "s"))
+        if speler == 3:
+            hover(label, "red", "#d6e0f5")
 
 def hand_maken(window, speler, hand):
     for kaart in range(1, len(hand) + 1):
-        open_image(window, "Images/{}".format(
+        label = open_image(window, "Images/{}".format(
             "Omgedraaide kaart.png" if (speler == 1) else "{}/{} ({}).png".format(hand[kaart - 1], hand[kaart - 1], 1)),
                    True if (speler == 1) else False, "place", kaart, speler, None, None, None)
+        if speler == 3:
+            hover(label, "red", "#d6e0f5")
 
 # Overige functies
 def kaart_van_trekstapel(trekstapel, aantal):
@@ -65,7 +73,7 @@ def instellen():
     trekstapel += ["SB"] * 18
     shuffle(trekstapel)
 
-    bouwstapels = {'A': [1, 2, 3], 'B': [1, 2, 3, 4, 5, 6], 'C': [1, 2, 3, 4, 5, 7, 8, 9], 'D': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+    bouwstapels = {'A': [1, 2, 3], 'B': [1, 2, 3, 4, 5, "SB"], 'C': [1, 2, 3, 4, 5, 7, 8, 9], 'D': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
     mens_weggooistapels = {'A': [9, 9], 'B': [10], 'C': [11, 11, 11], 'D': [12]}
     comp_weggooistapels = {'A': [1, 1], 'B': [2], 'C': [3, 3], 'D': [4]}
 
@@ -74,6 +82,16 @@ def instellen():
     mens_hand, trekstapel = kaart_van_trekstapel(trekstapel, 5)
 
     return trekstapel, bouwstapels, mens_weggooistapels, comp_weggooistapels, mens_stok, comp_stok, mens_hand
+
+def bovenste_kaart_bouwstapel(bouwstapel):
+    if not bouwstapel:
+        return 0
+    elif len(bouwstapel) == 1:
+        return 1
+    elif bouwstapel[-1] == "SB":
+        return bovenste_kaart_bouwstapel(bouwstapel[:-1]) + 1
+    else:
+        return bouwstapel[-1]
 
 # Main functie
 def run(window):
