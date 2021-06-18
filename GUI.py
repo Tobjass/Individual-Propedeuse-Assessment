5840db8e4a2e7390a0fc5188d9b682079fe1fded
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-18 17:35:47 +0200

    Update GUI.py
    mogelijkheid() toegevoegd zodat hover alleen werkt als de kaart op een bouwstapel kan worden geplaatst

--- GUI.py
+++ GUI.py
@@ -3,6 +3,12 @@ from PIL import ImageTk, Image
 from random import shuffle
 
 # Grafical User Interface
+def mogelijkheid(kaart, bouwstapels):
+    for stapel in bouwstapels:
+        if bovenste_kaart_bouwstapel(bouwstapels[stapel]) + 1 == kaart or kaart == "SB":
+            return True
+    return False
+
 def hover(label, on_hover, on_leave):
     label.bind("<Enter>", func=lambda x: label.config(background=on_hover))
     label.bind("<Leave>", func=lambda x: label.config(background=on_leave))
@@ -19,13 +25,15 @@ def open_image(window, bestand, rotate, setting, row, column, padx, pady, sticky
         label.place(x=70 * row if (column == 1) else 1320 + 73 * row, y=40 if (column == 1) else 780)
     return label
 
-def stapels_maken(window, speler, stapels, stok):
+def stapels_maken(window, speler, bouwstapels, stapels, stok):
+    print()
     for column in range(1, 9):
         window.columnconfigure(column, weight=1, minsize=75)
 
         temp_column = 9 - column if (speler != 3) else column
+        print(column, temp_column)
 
-        if temp_column == 1 or temp_column == 7 or temp_column == 8:
+        if temp_column in [1, 7, 8]:
             frame = tk.Frame(master=window, relief=tk.RAISED, background="#d6e0f5")
             frame.grid(row=speler, column=column, padx=5, pady=40, ipadx=82.5, ipady=118.5,
                        sticky="n" if (speler == 1) else (None if (speler == 2) else "s"))
@@ -44,6 +52,8 @@ def stapels_maken(window, speler, stapels, stok):
             label = open_image(window, "Images/{}/{} ({}).png".format(stok[0], stok[0], 5 if (len(stok) > 5) else len(stok)),
                        True if (speler == 1) else False, "grid", speler, column, 100, 40,
                        "n" if (speler == 1) else (None if (speler == 2) else "s"))
+            if speler == 3 and mogelijkheid(stok[0], bouwstapels):
+                hover(label, "red", "#d6e0f5")
         # Bouw en weggooistapels
         else:
             stapel = stapels[['A', 'B', 'C', 'D'][6 - (9 - column if (speler != 1) else column)]]
@@ -53,8 +63,8 @@ def stapels_maken(window, speler, stapels, stok):
                                                                                          stapel))),
                                True if (speler == 1) else False, "grid", speler, column, 5, 40,
                                "n" if (speler == 1) else (None if (speler == 2) else "s"))
-        if speler == 3:
-            hover(label, "red", "#d6e0f5")
+            if speler == 3 and mogelijkheid(stapel[-1], bouwstapels):
+                hover(label, "red", "#d6e0f5")
 
 def hand_maken(window, speler, hand):
     for kaart in range(1, len(hand) + 1):
@@ -73,8 +83,11 @@ def instellen():
     trekstapel += ["SB"] * 18
     shuffle(trekstapel)
 
+    lijst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "SB"]
+    shuffle(lijst)
+
     bouwstapels = {'A': [1, 2, 3], 'B': [1, 2, 3, 4, 5, "SB"], 'C': [1, 2, 3, 4, 5, 7, 8, 9], 'D': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-    mens_weggooistapels = {'A': [9, 9], 'B': [10], 'C': [11, 11, 11], 'D': [12]}
+    mens_weggooistapels = {'A': [lijst[0]], 'B': [lijst[1]], 'C': [lijst[2]], 'D': [lijst[3]]}
     comp_weggooistapels = {'A': [1, 1], 'B': [2], 'C': [3, 3], 'D': [4]}
 
     mens_stok, trekstapel = kaart_van_trekstapel(trekstapel, 30)
@@ -102,7 +115,7 @@ def run(window):
     for speler in range(1, 4):
         window.rowconfigure(speler, weight=1, minsize=50)
 
-        stapels_maken(window, speler,
+        stapels_maken(window, speler, bouwstapels,
                       comp_weggooistapels if (speler == 1) else (bouwstapels if (speler == 2) else mens_weggooistapels),
                       comp_stok if (speler == 1) else mens_stok)
 
