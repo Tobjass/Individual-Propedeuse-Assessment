d9841ff583bbff9884a9147f48c0dec34589c1dc
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-23 21:31:00 +0200

    Update GUI.py
    Try-except statement in on_drop() weer aangezet

--- GUI.py
+++ GUI.py
@@ -48,44 +48,44 @@ def on_drop(event, window, bouwstapels, mens_weggooistapels, comp_weggooistapels
     x, y = event.widget.winfo_pointerxy()
     target = event.widget.winfo_containing(x, y)
 
-    # try:
-    target.configure(image=event.widget.cget("image"))
+    try:
+        target.configure(image=event.widget.cget("image"))
 
-    x, y = position()
-    index, stapel = check_widget(mens_hand, event.widget.winfo_x(), x, y)
+        x, y = position()
+        index, stapel = check_widget(mens_hand, event.widget.winfo_x(), x, y)
 
-    keys = ['A', 'B', 'C', 'D']
-    mens_beurt = True
+        keys = ['A', 'B', 'C', 'D']
+        mens_beurt = True
 
-    if index == 5:
-        if bovenste_kaart_bouwstapel(bouwstapels[keys[stapel]]) + 1 == mens_stok[0] or mens_stok[0] == "SB":
-            print("(Speler)   Kaart {} uit stok naar bouwstapel {}".format(mens_stok[0], keys[stapel]))
-            bouwstapels[keys[stapel]].append(mens_stok[0])
-            mens_stok = mens_stok[1:]
-    elif index >= 6:
-        if bovenste_kaart_bouwstapel(bouwstapels[keys[stapel]]) + 1 == mens_weggooistapels[keys[index - 6]][-1] or \
-                mens_weggooistapels[keys[index - 6]][-1] == "SB":
-            print("(Speler)   Kaart {} uit weggooistapel {} naar bouwstapel {}"
-                  .format(mens_weggooistapels[keys[index - 6]][-1], keys[index - 6], keys[stapel]))
-            bouwstapels[keys[stapel]].append(mens_weggooistapels[keys[index - 6]][-1])
-            mens_weggooistapels[keys[index - 6]] = mens_weggooistapels[keys[index - 6]][:-1]
-    elif stapel <= 3 and index < 5:
-        if bovenste_kaart_bouwstapel(bouwstapels[keys[stapel]]) + 1 == mens_hand[index] or mens_hand[index] == "SB":
-            print("(Speler)   Kaart {} uit hand naar bouwstapel {}".format(mens_hand[index], keys[stapel]))
-            bouwstapels[keys[stapel]].append(mens_hand[index])
+        if index == 5:
+            if bovenste_kaart_bouwstapel(bouwstapels[keys[stapel]]) + 1 == mens_stok[0] or mens_stok[0] == "SB":
+                print("(Speler)   Kaart {} uit stok naar bouwstapel {}".format(mens_stok[0], keys[stapel]))
+                bouwstapels[keys[stapel]].append(mens_stok[0])
+                mens_stok = mens_stok[1:]
+        elif index >= 6:
+            if bovenste_kaart_bouwstapel(bouwstapels[keys[stapel]]) + 1 == mens_weggooistapels[keys[index - 6]][-1] or \
+                    mens_weggooistapels[keys[index - 6]][-1] == "SB":
+                print("(Speler)   Kaart {} uit weggooistapel {} naar bouwstapel {}"
+                      .format(mens_weggooistapels[keys[index - 6]][-1], keys[index - 6], keys[stapel]))
+                bouwstapels[keys[stapel]].append(mens_weggooistapels[keys[index - 6]][-1])
+                mens_weggooistapels[keys[index - 6]] = mens_weggooistapels[keys[index - 6]][:-1]
+        elif stapel <= 3 and index < 5:
+            if bovenste_kaart_bouwstapel(bouwstapels[keys[stapel]]) + 1 == mens_hand[index] or mens_hand[index] == "SB":
+                print("(Speler)   Kaart {} uit hand naar bouwstapel {}".format(mens_hand[index], keys[stapel]))
+                bouwstapels[keys[stapel]].append(mens_hand[index])
+                mens_hand = mens_hand[:index] + mens_hand[index + 1:]
+        elif 4 <= stapel <= 7 and index < 5:
+            mens_beurt = False
+            print("(Speler)   Kaart {} uit hand naar weggooistapel {}\n".format(mens_hand[index], keys[stapel - 4]))
+            mens_weggooistapels[keys[stapel - 4]].append(mens_hand[index])
             mens_hand = mens_hand[:index] + mens_hand[index + 1:]
-    elif 4 <= stapel <= 7 and index < 5:
-        mens_beurt = False
-        print("(Speler)   Kaart {} uit hand naar weggooistapel {}\n".format(mens_hand[index], keys[stapel - 4]))
-        mens_weggooistapels[keys[stapel - 4]].append(mens_hand[index])
-        mens_hand = mens_hand[:index] + mens_hand[index + 1:]
 
-    window.destroy()
-    window = tk.Tk(className=' Skip-Bo')
-    update_gui(window, bouwstapels, mens_weggooistapels, comp_weggooistapels, mens_stok, comp_stok, mens_hand,
-               comp_hand, mens_beurt, trekstapel)
-    # except:
-    #     pass
+        window.destroy()
+        window = tk.Tk(className=' Skip-Bo')
+        update_gui(window, bouwstapels, mens_weggooistapels, comp_weggooistapels, mens_stok, comp_stok, mens_hand,
+                   comp_hand, mens_beurt, trekstapel)
+    except:
+        pass
 
 """
 Maakt de achtergrond van een kaart rood als de muis er op zit.
