d97183c7f96bf7ad6fc57b836ffa2fe0842c521f
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-06-23 19:14:57 +0200

    Update Unit-testing.py
    kaart_van_trekstapel() test toegevoegd

--- Unit-testing.py
+++ Unit-testing.py
@@ -14,6 +14,9 @@ def mogelijkheid(kaart, bouwstapels):
             return True
     return False
 
+def kaart_van_trekstapel(trekstapel, aantal):
+    return trekstapel[:aantal], trekstapel[aantal:]
+
 def test_bovenste_kaart_bouwstapel():
     assert bovenste_kaart_bouwstapel([1, 2, 3, "SB"]) == 4, "Moet 4 zijn"
 
@@ -23,6 +26,22 @@ def test_mogelijkheid():
     assert mogelijkheid(10, {'A': [1, 2], 'B': [1, 2, 3, 4, 5, 6], 'C': [1, 2, "SB"],
                              'D': [1, 2, 3, 4, "SB"]}) is False, "Moet False zijn"
 
+def test_kaart_van_trekstapel():
+    trekstapel = [6, 7, 'SB', 6, 'SB', 7, 8, 6, 12, 4, 4, 'SB', 10, 5, 3, 6, 6, 1, 9, 'SB', 1, 1, 5, 7, 'SB', 3, 1, 4,
+                  8, 7, 12, 8, 10, 9, 2, 1, 2, 10, 5, 1, 8, 10, 1, 5, 9, 2, 11, 4, 10, 7, 7, 'SB', 8, 'SB', 3, 8, 5,
+                  'SB', 6, 10, 11, 12, 9, 6, 'SB', 3, 10, 9, 4, 2, 11, 7, 1, 8, 8, 9, 11, 6, 3, 12, 4, 2, 'SB', 4, 8, 1,
+                  5, 3, 3, 'SB', 7, 12, 6, 2, 9, 12, 'SB', 9, 2, 3, 9, 9, 12, 12, 10, 11, 11, 12, 'SB', 'SB', 5, 4, 11,
+                  10, 5, 2, 7, 3, 4, 'SB', 7, 11, 11, 1, 10, 3, 3, 11, 12, 5, 8, 10, 1, 5, 9, 5, 'SB', 12, 7, 7, 4, 6,
+                  6, 3, 11, 2, 'SB', 10, 8, 5, 12, 8, 11, 2, 6, 1, 'SB', 4, 2, 4, 2, 9]
+    stok = [6, 7, 'SB', 6, 'SB', 7, 8, 6, 12, 4, 4, 'SB', 10, 5, 3, 6, 6, 1, 9, 'SB', 1, 1, 5, 7, 'SB', 3, 1, 4, 8, 7]
+    nieuwe_trekstapel = [12, 8, 10, 9, 2, 1, 2, 10, 5, 1, 8, 10, 1, 5, 9, 2, 11, 4, 10, 7, 7, 'SB', 8, 'SB', 3, 8, 5,
+                         'SB', 6, 10, 11, 12, 9, 6, 'SB', 3, 10, 9, 4, 2, 11, 7, 1, 8, 8, 9, 11, 6, 3, 12, 4, 2, 'SB',
+                         4, 8, 1, 5, 3, 3, 'SB', 7, 12, 6, 2, 9, 12, 'SB', 9, 2, 3, 9, 9, 12, 12, 10, 11, 11, 12, 'SB',
+                         'SB', 5, 4, 11, 10, 5, 2, 7, 3, 4, 'SB', 7, 11, 11, 1, 10, 3, 3, 11, 12, 5, 8, 10, 1, 5, 9, 5,
+                         'SB', 12, 7, 7, 4, 6, 6, 3, 11, 2, 'SB', 10, 8, 5, 12, 8, 11, 2, 6, 1, 'SB', 4, 2, 4, 2, 9]
+    assert kaart_van_trekstapel(trekstapel, 30) == (stok, nieuwe_trekstapel), "Klopt niet"
+
 test_bovenste_kaart_bouwstapel()
 test_mogelijkheid()
+test_kaart_van_trekstapel()
 print("Tests succesvol")
